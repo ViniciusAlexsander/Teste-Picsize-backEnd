037142f9da649e8a1b0f649f9cec6b01c52c3b26
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2020-07-04 18:15:04 -0300

    Conversão da api para salvar os dados em arquivo json

--- src/app/controllers/loanController.js
+++ src/app/controllers/loanController.js
@@ -1,8 +1,10 @@
 const loanModel = require("../models/loanModel");
 const moment = require("moment");
+const fs = require("fs");
+const data = require("../../../data.json");
 
 module.exports = {
-  async create(req, res) {
+  create(req, res) {
     try {
       const {
         cpf,
@@ -15,8 +17,18 @@ module.exports = {
       } = req.body;
 
       let simulationDay = moment().format();
+      let lastId;
 
-      const userId = await loanModel.insert({
+      if (data.loans[0] == undefined) {
+        lastId = 0;
+      } else {
+        lastId = data.loans[data.loans.length - 1].id;
+      }
+
+      const id = Number(lastId + 1);
+
+      data.loans.push({
+        id,
         cpf,
         uf,
         birth,
@@ -27,7 +39,14 @@ module.exports = {
         simulationDay,
       });
 
-      return res.json(userId);
+      fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err)
+          return res.status(400).json({
+            error:
+              "Algum erro inesperado aconteceu, tente novamente mais tarde!",
+          });
+        return res.json({ data });
+      });
     } catch (error) {
       console.error(error);
       return res.status(400).json({
--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-
-module.exports = new Pool({
-  user: "postgres",
-  password: "vinicius25",
-  host: "localhost",
-  port: 5432,
-  database: "testePicsize",
-});
