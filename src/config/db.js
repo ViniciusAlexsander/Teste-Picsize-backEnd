d54f7afa64dcfd7eeeca1521565538229ac55b30
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2020-07-03 22:33:27 -0300

    Adicionado o procfile

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 
 module.exports = new Pool({
-  user: 'postgres',
-  password: "vinicius25",
-  host: "localhost",
+  user: "mtixayuauomtar",
+  password: "eb753a1a9f1dec96289ea6108e493c2381050c403e9698ce527416c94f510732",
+  host: "ec2-52-0-155-79.compute-1.amazonaws.com",
   port: 5432,
-  database: "testePicsize" 
-})
+  database: "d1e715k0kd028o",
+});
